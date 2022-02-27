c4090dfca259e673117f416a1d1cd8c365bcc06f
Sara Mazal <32996161+saramazal@users.noreply.github.com>
2022-02-27 18:01:43 +0200

    adding files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Board from './components/board';
+import './components/style.css';
+
+function App() {
+  return (
+    <div className="App" id='main'>
+        <Board />
+    </div>
+  );
+}
+
+export default App;
